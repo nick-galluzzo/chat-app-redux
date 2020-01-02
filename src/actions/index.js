557425da96241d7bbe66179c27a85c3123505f4a
Nick Galluzzo <n.galluzzo@gmail.com>
2020-01-02 13:12:58 -0500

    created fetchMessages action that pull from le wagon messages API

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,11 @@
-// TODO: add and export your own actions
+export const FETCH_MESSAGES = 'FETCH_MESSAGES';
+
+export const fetchMessages = (channel) => {
+  const promise = fetch(`https://wagon-chat.herokuapp.com/${channel}/messages`)
+    .then(response => response.json());
+
+  return {
+    type: FETCH_MESSAGES,
+    payload: promise
+  };
+};
