e51bfab9cab6b585f9b691861ab536b85ccf959d
Nick Galluzzo <n.galluzzo@gmail.com>
2020-01-02 16:37:22 -0500

    initialized a create message case function that returns the new message in state

--- src/reducers/messagesReducer.js
+++ src/reducers/messagesReducer.js
@@ -1,21 +1,20 @@
 import { FETCH_MESSAGES, CREATE_MESSAGE } from '../actions/index';
 
 const messagesReducer = (state = [], action) => {
-  if (state === undefined) {
+  if (state === null) {
     return [];
   }
 
   switch (action.type) {
-    case FETCH_MESSAGES:
-      let newState = state;
-      newState = action.payload.messages;
-
-      return newState;
-    case CREATE_MESSAGE:
-      newState = state;
+    case FETCH_MESSAGES: {
+      return action.payload.messages;
+    }
+    case CREATE_MESSAGE: {
+      const newState = state.slice(0);
       newState.push(action.payload);
 
       return newState;
+    }
     default:
       return state;
   }
