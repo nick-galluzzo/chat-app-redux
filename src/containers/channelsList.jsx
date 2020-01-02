ca57508ea47c8ac58d28626f359186541b6f02ac
Nick Galluzzo <n.galluzzo@gmail.com>
2020-01-02 18:02:51 -0500

    initialize

---
+++ src/containers/channelsList.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default class ChannelsList extends React.Component {
+  render() {
+    return (
+      <div className='channels-list'>
+        <div className="channels-list">
+        <h1>hi</h1>
+        </div>
+      </div>
+    );
+  }
+}
