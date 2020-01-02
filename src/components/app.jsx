506bf197445dafbbbff1d345f78a4cbfeabf872b
Nick Galluzzo <n.galluzzo@gmail.com>
2020-01-02 17:57:47 -0500

    added ChannelList container to app

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import MessageList from '../containers/messageList';
+import ChannelsList from '../containers/channelsList';
 
 const App = () => {
   return (
     <div className="app">
+      <ChannelsList />
       <MessageList />
     </div>
   );
