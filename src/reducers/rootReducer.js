6a9e4a3e03253284b55ec8bbee70580af7c7ca38
Nick Galluzzo <n.galluzzo@gmail.com>
2020-01-02 11:40:06 -0500

    change import type

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,16 +1,16 @@
 import { combineReducers } from 'redux';
-import { channelsReducer } from './channelsReducer';
-import { currentUserReducer } from './currentUserReducer';
-import { messagesReducer } from './messagesReducer';
-import { selectedChannelReducer } from './selectedChannelReducer';
+import  channelsReducer from './channelsReducer';
+import  currentUserReducer from './currentUserReducer';
+import  messagesReducer from './messagesReducer';
+import  selectedChannelReducer from './selectedChannelReducer';
 
 
-const initializeState = {
-  messages: [],
-  channels: ['general', 'nyc', 'redux'],
-  selectedChannel: 'nyc',
-  currentUser: prompt("What is your username?") || `anonymous${Math.floor(10 + (Math.random() * 90))}`
-};
+// const initializeState = {
+//   messages: [],
+//   channels: ['general', 'nyc', 'redux'],
+//   selectedChannel: 'nyc',
+//   currentUser: prompt("What is your username?") || `anonymous${Math.floor(10 + (Math.random() * 90))}`
+// };
 
 const rootReducer = combineReducers({
   channels: channelsReducer,
@@ -19,4 +19,4 @@ const rootReducer = combineReducers({
   selectedChannel: selectedChannelReducer
 });
 
-export default { rootReducer, initializeState };
+export default rootReducer;
