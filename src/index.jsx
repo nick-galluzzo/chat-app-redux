e33b6dec86b05e3752eebdc5ff79b68a0d257556
Nick Galluzzo <n.galluzzo@gmail.com>
2020-01-02 12:18:48 -0500

    formatting

--- src/index.jsx
+++ src/index.jsx
@@ -2,15 +2,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, combineReducers } from 'redux';
 import store from './components/store';
 
 // internal modules
 import App from './components/app';
 import '../assets/stylesheets/application.scss';
-
-// State and reducers
-
 
 // render an instance of the component in the DOM
 ReactDOM.render(
