f478a0ed6efd129886c0b5a16028202e0154f753
Nick Galluzzo <n.galluzzo@gmail.com>
2020-01-02 18:30:48 -0500

    map channel and selected channel state to props and set styling based on selected channel

--- src/containers/channelsList.jsx
+++ src/containers/channelsList.jsx
@@ -1,13 +1,45 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 
-export default class ChannelsList extends React.Component {
+class ChannelsList extends React.Component {
+  getStyle = (channel) => {
+    if (channel === this.props.selectedChannel) {
+      return {
+        color: 'green',
+        cursor: 'default',
+        fontStyle: 'bold',
+        opacity: '.6'
+      };
+    }
+  }
+
+  renderChannels() {
+    const { channels } = this.props;
+    return channels.map((channel) => {
+      return <li style={this.getStyle(channel)}>{channel}</li>;
+    });
+  }
+
+
   render() {
     return (
       <div className='channels-list'>
         <div className="channels-list">
-        <h1>hi</h1>
+        <h4>{this.props.selectedChannel} chat</h4>
+          <ul>
+            { this.renderChannels() }
+          </ul>
         </div>
       </div>
     );
   }
 }
+
+const mapStateToProps = (state) => ({
+  channels: state.channels,
+  selectedChannel: state.selectedChannel
+});
+
+export default connect(mapStateToProps)(ChannelsList);
+
