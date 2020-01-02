046bb9b7ad772e2c9d0606615ceebe37b9a551c7
Nick Galluzzo <n.galluzzo@gmail.com>
2020-01-02 15:07:26 -0500

    create form for inputting messages

---
+++ src/containers/messageForm.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import { createMessage } from '../actions/index';
+
+class MessageForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      content: ''
+    };
+  }
+
+  handleMessage(property) {
+    return e => this.setState({ [property]: e.target.value });
+  }
+
+  handleSubmit = () => {
+    this.props.createMessage(this.props.selectedChannel, this.props.currentUser, this.state.content);
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input
+          type="text"
+          value={this.state.content}
+          placeholder="Write your message here..."
+          onChange={this.handleMessage('content')}
+        />
+        <button>
+          Send Message
+        </button>
+      </form>
+    );
+  }
+}
+
+const mapStateToProps = (state) => ({
+  currentUser: state.currentUser,
+  selectedChannel: state.selectedChannel
+});
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ createMessage }, dispatch);
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(MessageForm);
