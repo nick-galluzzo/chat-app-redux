51dc32f33c71bc7b997a3f74eb3de5520175400e
Nick Galluzzo <n.galluzzo@gmail.com>
2020-01-03 11:45:47 -0500

    export SELECT_CHANNEL const and initialize selectChannel function returning a payload of selected channel

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,6 @@
 export const FETCH_MESSAGES = 'FETCH_MESSAGES';
 export const CREATE_MESSAGE = 'CREATE_MESSAGE';
+export const SELECT_CHANNEL = 'SELECT_CHANNEL';
 
 export const fetchMessages = (channel) => {
   const promise = fetch(`https://wagon-chat.herokuapp.com/${channel}/messages`)
@@ -29,4 +30,11 @@ export const createMessage = (channel, author, content) => {
     payload: promise
   };
 };
+
+export function selectChannel(channel) {
+  return {
+    type: SELECT_CHANNEL,
+    payload: channel
+  };
+}
 
