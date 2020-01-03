99d2a69afdbed015cb6b4e7f16f78afd37e824a4
Nick Galluzzo <n.galluzzo@gmail.com>
2020-01-03 13:52:30 -0500

    initialize askUsername action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,7 @@
 export const FETCH_MESSAGES = 'FETCH_MESSAGES';
 export const CREATE_MESSAGE = 'CREATE_MESSAGE';
 export const SELECT_CHANNEL = 'SELECT_CHANNEL';
+export const ASK_USERNAME = 'ASK_USERNAME';
 
 export const fetchMessages = (channel) => {
   const promise = fetch(`https://wagon-chat.herokuapp.com/${channel}/messages`)
@@ -37,4 +38,11 @@ export function selectChannel(channel) {
     payload: channel
   };
 }
+
+export const askUsername = () => {
+  return {
+    type: ASK_USERNAME,
+    payload: prompt('What is your username?')
+  };
+};
 
