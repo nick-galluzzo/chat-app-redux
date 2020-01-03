ea3f69321db5fbdc39c8ed257a3be30a5c46b257
Nick Galluzzo <n.galluzzo@gmail.com>
2020-01-03 11:46:13 -0500

    formatting

--- src/components/store.js
+++ src/components/store.js
@@ -3,6 +3,7 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import reduxPromise from 'redux-promise';
 import rootReducer from '../reducers/rootReducer';
 
+
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const middlewares = composeEnhancers(applyMiddleware(logger, reduxPromise));
 
