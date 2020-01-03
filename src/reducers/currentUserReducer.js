a944c35ba4ea7e242b9b37770352485de6ae2f1b
Nick Galluzzo <n.galluzzo@gmail.com>
2020-01-03 13:54:13 -0500

    update state with prompted username

--- src/reducers/currentUserReducer.js
+++ src/reducers/currentUserReducer.js
@@ -1,5 +1,8 @@
-const currentUserReducer = (state = 'Nick', action) => {
+import { ASK_USERNAME } from '../actions/index';
+const currentUserReducer = (state = null, action) => {
   switch (action.type) {
+    case ASK_USERNAME:
+      return action.payload;
     default:
       return state;
   }
