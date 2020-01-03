f8aa823d0e660c01e060b59c56f38df6f22e1a70
Nick Galluzzo <n.galluzzo@gmail.com>
2020-01-03 14:56:07 -0500

    add unique colors to usernames

--- src/components/message.jsx
+++ src/components/message.jsx
@@ -1,5 +1,16 @@
 import React from 'react';
 
+function strToRGB(str) {
+  let hash = 0;
+  for (let i = 0; i < str.length; i += 1) {
+    hash = str.charCodeAt(i) + ((hash << 5) - hash);
+  }
+  const c = (hash & 0x00FFFFFF)
+    .toString(16)
+    .toUpperCase();
+  return `#${"00000".substring(0, 6 - c.length)}${c}`;
+}
+
 export default class Message extends React.Component {
   constructor(props) {
     super(props);
@@ -9,7 +20,7 @@ export default class Message extends React.Component {
     const { message } = this.props;
     return (
       <div className="messages">
-        <h4> {message.author} </h4>
+        <h4 style={{ color: strToRGB(message.author)}}> {message.author} </h4>
         <p> {message.content} </p>
       </div>
     );
