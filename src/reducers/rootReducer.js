14343e99fe1a1cc46335ff213ff6ceb65cbd994f
Nick Galluzzo <n.galluzzo@gmail.com>
2020-01-03 11:48:13 -0500

    reformat code and remove initialize state

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,15 +1,10 @@
 import { combineReducers } from 'redux';
-import  channelsReducer from './channelsReducer';
-import  currentUserReducer from './currentUserReducer';
-import  messagesReducer from './messagesReducer';
-import  selectedChannelReducer from './selectedChannelReducer';
+import channelsReducer from './channelsReducer';
+import currentUserReducer from './currentUserReducer';
+import messagesReducer from './messagesReducer';
+import selectedChannelReducer from './selectedChannelReducer';
 
-const initializeState = {
-  messages: [],
-  channels: ['general', 'nyc', 'redux'],
-  selectedChannel: 'nyc',
-  currentUser: prompt("What is your username?") || `anonymous${Math.floor(10 + (Math.random() * 90))}`
-};
+
 
 const rootReducer = combineReducers({
   channels: channelsReducer,
