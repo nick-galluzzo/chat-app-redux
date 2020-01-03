ccf9b187b1d35ada8dad57f94cb7ba4e017e5e81
Nick Galluzzo <n.galluzzo@gmail.com>
2020-01-03 11:48:59 -0500

    change import style and selectChannelReducer returns action payload

--- src/reducers/selectedChannelReducer.js
+++ src/reducers/selectedChannelReducer.js
@@ -1,8 +1,14 @@
-const selectedChannelReducer = (state = 'general', action) => {
+import { SELECT_CHANNEL } from '../actions/index';
+
+function selectedChannelReducer (state = 'nyc', action) {
   switch (action.type) {
-    default:
+    case SELECT_CHANNEL: {
+      return action.payload;
+    }
+    default: {
       return state;
+    }
   }
-};
+}
 
 export default selectedChannelReducer;
