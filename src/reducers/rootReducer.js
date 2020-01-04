8c9380c28f80751651a45b2d8daddeba9fef2769
Nick Galluzzo <n.galluzzo@gmail.com>
2020-01-04 11:25:49 -0500

    remove selectedChannel as a reducer

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -2,13 +2,13 @@ import { combineReducers } from 'redux';
 import channelsReducer from './channelsReducer';
 import currentUserReducer from './currentUserReducer';
 import messagesReducer from './messagesReducer';
-import selectedChannelReducer from './selectedChannelReducer';
+// import selectedChannelReducer from './selectedChannelReducer';
 
 const rootReducer = combineReducers({
   channels: channelsReducer,
   currentUser: currentUserReducer,
   messages: messagesReducer,
-  selectedChannel: selectedChannelReducer
+  // selectedChannel: selectedChannelReducer
 });
 
 export default rootReducer;
